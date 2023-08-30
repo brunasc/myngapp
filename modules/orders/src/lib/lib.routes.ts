e2c4c88bbbf533e39f99ee83fca8406c328f2643
Bruna Souza Chagas <bruna.chagass@hotmail.com>
2023-08-29 21:45:56 -0300

    feat: criação e configuração inicial do projeto e módulos

---
+++ modules/orders/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { OrderListComponent } from './order-list/order-list.component';
+
+export const ordersRoutes: Route[] = [
+   {path: '', pathMatch: 'full', component: OrderListComponent} 
+];
